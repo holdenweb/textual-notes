fa05d4e24b254d11aedc5d88d2fde1d2793b53c5
Steve Holden <steve@holdenweb.com>
2025-01-20 15:07:29 +0000

    Create branching point to reduce complexity.
    I really need to clarify the separation between the
    interface and the objects, while leaving control of
    the individual operations in the data object.
    
    As a general rule, operations on an object should be
    methods of the object.

--- notes.py
+++ notes.py
@@ -6,6 +6,7 @@ from textual.containers import Center
 from textual.containers import Horizontal
 from textual.containers import Vertical
 from textual.widgets import Button
+from textual.widgets import Input
 from textual.widgets import Label
 from textual.widgets import Placeholder
 from textual.widgets import Select
@@ -17,21 +18,25 @@ class NoteForm(Vertical):
     DEFAULT_CSS = """
 """
 
-    def __init__(self, db, otyp=None, *args, **kwargs):
+    def __init__(self, db, argd=None, *args, **kwargs):
+        self.argd = argd
         self.db = db
         super().__init__(*args, **kwargs)
 
     def compose(self) -> ComposeResult:
         self.btn: Button = Button("Submit")
+        ots = Select([], id="otyp-sel")
+        ots.border_title = "Select object type"
+        ins = Input(id="inst-sel")
         with Vertical():
-            yield Horizontal(
-                Select([], id="otyp-sel"), Placeholder(id="inst-sel"), id="selections"
-            )
+            yield Horizontal(ots, ins, id="selections")
             with Horizontal(id="status"):
-                if not self.app.otyp:
-                    yield Splash()
+                if not self.argd.type:
+                    yield Splash("Please select an object type above")
                 else:
-                    yield NoteEditor()
+                    yield NoteEditor()  # Representing --type=notes --op=edit
+                    # but with no instance selected
+                    # pretty sure I'm grasping for a generality here ...
 
 
 class NoteEditor(Horizontal):
@@ -45,20 +50,25 @@ class NoteEditor(Horizontal):
 
 
 class Splash(Center):
+    def __init__(self, message, *args, **kwargs):
+        self.message = message
+        super().__init__(*args, classes="splash-holder", **kwargs)
+
     def compose(self):
-        yield Label("Select an object type to edit")
+        with Center(classes="splash-holder"):
+            yield Label(self.message)
 
 
 class NoteApp(App):
     CSS_PATH = "x.tcss"
 
-    def __init__(self, otyp, *args, **kwargs):
+    def __init__(self, argd, *args, **kwargs):
         self.db = db.DB("project_notes")  # This could become a CLI option
-        self.otyp = otyp
+        self.argd = argd
         super().__init__(*args, **kwargs)
 
     def compose(self) -> ComposeResult:
-        yield NoteForm(self.db, otyp=self.otyp, id="main-window")
+        yield NoteForm(self.db, argd=self.argd, id="main-window")
 
     def on_click(self, event):
         self.log(self.tree)
@@ -70,9 +80,12 @@ def main():
     parser.add_argument(
         "--type", default=None, help="The object type to be maintained."
     )
+    parser.add_argument(
+        "--inst", default=None, help="The key of the instance to be maintained."
+    )
     args = parser.parse_args()
     print(args)
-    app = NoteApp(otyp=args.type)
+    app = NoteApp(argd=args)
     app.run()
 
 
